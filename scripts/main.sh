4590cf2d8f2c6f50c22245a3095a047d086f5ea8
JM-Lazaro <30991348+JM-Lazaro@users.noreply.github.com>
2020-11-16 19:59:17 +1100

    Create main.sh

---
+++ scripts/main.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+while true; do
+
+## DOGSTATSD
+if [[ $DOGSTATSD_ENABLE == "true" ]] && [[ -n ${DOGSTATSD_HOST} ]]; then
+echo -n "$DOGSTATSD_NAMESPACE.custom_metric:$DOGSTATSD_VALUE|$DOGSTATSD_METRIC_TYPE|#$DOGSTATSD_TAGS" | nc -v -u -w 1 $DOGSTATSD_HOST 8125
+fi
+
+## LOG
+if [[ $LOGS_ENABLE == "true" ]]; then
+# echo $LOGS_LINE >> /logs/script.log;
+echo $LOGS_LINE;
+fi
+
+## APM
+if [[ $APM_ENABLE == "true" ]] && [[ -n ${APM_AGENT_HOST} ]]; then
+/scripts/apm.sh
+fi
+
+## SLEEP
+sleep $SLEEP;
+
+
+done

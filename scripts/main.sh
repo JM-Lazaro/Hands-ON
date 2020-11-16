65676a28f688807b7b6f85b67ccb3bb8fe89893d
JM-Lazaro <30991348+JM-Lazaro@users.noreply.github.com>
2020-11-16 20:31:58 +1100

    Update main.sh

--- scripts/main.sh
+++ scripts/main.sh
@@ -4,12 +4,11 @@ while true; do
 
 ## DOGSTATSD
 if [[ $DOGSTATSD_ENABLE == "true" ]] && [[ -n ${DOGSTATSD_HOST} ]]; then
-echo -n "$DOGSTATSD_NAMESPACE.custom_metric:$DOGSTATSD_VALUE|$DOGSTATSD_METRIC_TYPE|#$DOGSTATSD_TAGS" | nc -v -u -w 1 $DOGSTATSD_HOST 8125
+echo -n "$DOGSTATSD_NAMESPACE.custom_metric:$DOGSTATSD_VALUE|$DOGSTATSD_METRIC_TYPE|#$DOGSTATSD_TAGS" | nc -v -u -w 1 $DOGSTATSD_HOST 8125;
 fi
 
 ## LOG
 if [[ $LOGS_ENABLE == "true" ]]; then
-# echo $LOGS_LINE >> /logs/script.log;
 echo $LOGS_LINE;
 fi
 
